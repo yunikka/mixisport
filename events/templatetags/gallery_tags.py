b7a1b73f352e8373b1393399ab556d69128b1f98
Yuriy Kazankin <yuriy.kazankin@homecredit.ru>
2017-07-12 23:30:42 +0300

    Добавлен список галлерей на главной

---
+++ events/templatetags/gallery_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from photologue.models import Gallery
+from mixisport.settings import MEDIA_URL
+
+register = template.Library() 
+
+@register.inclusion_tag("gallery_tags.html")
+def show_gallery_tags():
+    galleries = Gallery.objects.all()[0:2]
+    return {'galleries': galleries, 'MEDIA_URL': MEDIA_URL}
