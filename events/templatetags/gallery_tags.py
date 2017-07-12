b26517d340bad60cbdeaebdb6ba752de20e0635f
Yuriy Kazankin <yuriy.kazankin@homecredit.ru>
2017-07-12 23:33:26 +0300

    Добавлен список галлерей на главной

--- events/templatetags/gallery_tags.py
+++ events/templatetags/gallery_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.inclusion_tag("gallery_tags.html")
 def show_gallery_tags():
-    galleries = Gallery.objects.all()[0:2]
+    galleries = Gallery.objects.all()[0:3]
     return {'galleries': galleries, 'MEDIA_URL': MEDIA_URL}
